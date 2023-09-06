c5a1a8914b7e3f1d74640dbd159d8431c36e610a
iosif <tamas2792@gmail.com>
2023-09-06 15:37:05 +0300

    updated token manager

--- src/test/java/com/spotify/oauth2/api/TokenManager.java
+++ src/test/java/com/spotify/oauth2/api/TokenManager.java
@@ -25,6 +25,7 @@ public class TokenManager {
                 System.out.println("Token is good to use!");
             }
         } catch (Exception e) {
+            e.printStackTrace();
             throw new RuntimeException("ABORT!!! Failed to get token!");
         }
         return access_token;
